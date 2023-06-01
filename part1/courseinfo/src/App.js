1fe10eb7ce02c31bc1cb3b3e629ba1537183224e
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2023-06-01 17:28:27 +0800

    sections 1.1 and 1.2 finished

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,59 @@
+
+const Header = (props) =>{
+  return (
+    <div>
+     <h1>{props.course}</h1>
+    </div>
+    )
+}
+
+const Part = (props) =>{
+  console.log(props)
+  return (
+    <>
+      <p>{props.parts.name} {props.parts.exercise}</p>
+    </>
+  )
+  }
+
+const Content = (props) =>{
+  console.log(props)
+return (
+  <div>
+    <Part parts={props.parts[0]}  />
+    <Part parts={props.parts[1]}  />
+    <Part parts={props.parts[2]}  />
+  </div>
+)
+}
+
+const Total = (props) =>{
+
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course ={ 
+    name: 'Half Stack application development',
+     parts: [
+    {name: 'Fundamentals of React', exercise: 10},
+    {name: 'Using props to pass data', exercise: 7},
+    {name: 'State of a component', exercise: 14},
+  ]
+}
+
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts = {course.parts} />
+      <Total total={course.parts[0].exercise+course.parts[1].exercise+course.parts[2].exercise} />
+    </div>
+  )
+}
+
+export default App
