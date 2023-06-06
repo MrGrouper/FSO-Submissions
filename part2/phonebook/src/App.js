65e8f9f170fc56caf678818e2d0a77bef110fdcb
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2023-06-06 20:06:42 +0800

    Finished 2.6-2.10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import Name from "./components/Name";
+
+const Filter = (props) => {
+  console.log(props);
+  return (
+    <div>
+      filter shown with:
+      <input value={props.newSearch} onChange={props.handleSearch} />
+    </div>
+  );
+};
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.addName}>
+      <div>
+        name:
+        <input value={props.newName} onChange={props.handleNameChange} />
+      </div>
+      <div>
+        number:
+        <input value={props.newPhone} onChange={props.handlePhoneChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const App = (props) => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-1234567" },
+  ]);
+
+  const [newName, setNewName] = useState("");
+
+  const [newPhone, setNewPhone] = useState("");
+
+  const [newSearch, setNewSearch] = useState("");
+
+  const addName = (event) => {
+    const found = persons.find(
+      (element) => JSON.stringify(element.name) === JSON.stringify(newName)
+    );
+
+    if (found) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      event.preventDefault();
+      const personObj = {
+        name: newName,
+        phone: newPhone,
+      };
+      setNewName("");
+      setNewPhone("");
+      setPersons(persons.concat(personObj));
+    }
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value);
+    setNewPhone(event.target.value);
+  };
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    setNewSearch(event.target.value);
+  };
+
+  const search = persons.filter((str) =>
+    str.name.toLowerCase().includes(newSearch.toLowerCase())
+  );
+
+  const personsToShow = search ? search : persons;
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter newSearch={newSearch} handleSearch={handleSearch} />
+
+      <h3>add a new</h3>
+
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newPhone={newPhone}
+        handlePhoneChange={handlePhoneChange}
+      />
+
+      <h3>Numbers</h3>
+      
+        {personsToShow.map((person) => (
+          
+            <Name key={person.name} name={person.name} phone={person.phone} />
+          
+        ))}
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/phonebook/src/components/Name.js
@@ -0,0 +1,8 @@
+const Name = (props) => {
+
+    return (
+      <p>{props.name} {props.phone}</p>
+    )
+  }
+  
+  export default Name
